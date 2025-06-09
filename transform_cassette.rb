956893946708a13ce42e1759794334d296d0c91f
Thomas Harbert <tgharbert3@gmail.com>
2025-06-09 09:43:33 -0500

    cleaned up

--- transform_cassette.rb
+++ transform_cassette.rb
@@ -8,20 +8,24 @@ data = YAML.load_file(input_file)
 # Keep only the first http_interaction
 data['http_interactions'] = [data['http_interactions'].first]
 
+# Grabs only the first response - no pagination with nillifying the next_cursor
 first_interaction = data['http_interactions'].first
 json_str = first_interaction['response']['body']['string']
 first_interaction['']
 parsed_json = JSON.parse(json_str)
-parsed_json["next_cursor"] = nil
-limit_count = 3
-parsed_json['items'] = parsed_json['items'].first(limit_count)
 
-# this can be used to extract Ruby hash for the spec file test of item[0]
-# grab this value from the console output and use it in the spec file
+parsed_json["next_cursor"] = nil
+
+# How many items we want to keep in the parsed JSON for the truncated cassette
+item_limit = 3
+parsed_json['items'] = parsed_json['items'].first(item_limit)
+
+# NOTE: this can be used to extract Ruby hash for the spec file test of item[0]
+# Uncomment the below and grab printed ruby hash from the console output and use it in the spec file
 # puts "Parsed JSON: #{parsed_json['items'][0]}"
 
 first_interaction['response']['body']['string'] = JSON.generate(parsed_json)
 # puts first_interaction['response']['body']['string']
 
-# File.write(output_file, data.to_yaml)
+# line_width: -1 is used to avoid wrapping long lines in the YAML output - puts it on a single line
 File.write(output_file, data.to_yaml(line_width: -1))
